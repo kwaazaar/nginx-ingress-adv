dbda7250b29c757a2d0560d4aee0ec4b7bb5c7e3
Robert te Kaat <hvkaat@aahg.onmicrosoft.com>
2019-06-13 08:39:38 +0200

    egress

--- listctl.sh
+++ listctl.sh
@@ -1,3 +1,3 @@
-echo "pod,namespace,phase,container-image,initcontainer-image" > containers.csv
-kubectl get pods --all-namespaces -o=jsonpath='{range .items[*]}{.metadata.name}{","}{..namespace}{","}{..status.phase}{","}{..status.containerStatuses[].image}{","}{..status.initContainerStatuses[].image}{"\n"}{end}' >> containers.csv
+echo "pod,namespace,phase,container-image,initcontainer-image"
+kubectl get pods --all-namespaces -o=jsonpath='{range .items[*]}{.metadata.name}{","}{..namespace}{","}{..status.phase}{","}{..status.containerStatuses[].image}{","}{..status.initContainerStatuses[].image}{"\n"}{end}'
 
